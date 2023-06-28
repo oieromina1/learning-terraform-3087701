6c2286c69aa5182354ab2632cf35ddd3de65397d
oieromina1 <137255898+oieromina1@users.noreply.github.com>
2023-06-28 23:00:27 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,15 +11,16 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.10.0"
   name = "blog"
-min_size = 1
-max_size = 2
+min_size = var.min_size
+max_size = var.max_size
+
 Vpc_zone_identifier = module.blog_vpc.public_subnets
 target_group_arns = module.blog_alb.target_group_arns
 security_groups = [module.blog_sg.security_group_id]
@@ -30,7 +31,6 @@ instance_type = "var.instance_type"
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
-
   name = "blog-alb"
 
   load_balancer_type = "application"
@@ -69,20 +69,20 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  module "vpc" {
+  module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "dev"
-  cidr = "10.0.0.0/16"
+  name = var.environment.name
+  cidr = "${var.environment.network_prefix}0.0/16"
 
   azs             = ["us-east-2", "eu-west-1b", "us-east-2a"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}101.0/24", "${var.environment.network_prefix}102.0/24", "${var.environment.network_prefix}103.0/24"]
 
   enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
-    Environment = "dev"
+    Environment = var.environment.name
   }
 }
 ingress_rules      = ["http-80-tcp", "https-443-tcp"]
